6d96a1cc5c990b514e13998488226c3691bf19cd
DevJLeon <celisjoshleon@gmail.com>
2023-11-16 17:18:39 -0500

    feat: :construction: Repositories

--- Application/Repository/RolRepository.cs
+++ Application/Repository/RolRepository.cs
@@ -10,7 +10,7 @@ public class RolRepository : GenericRepo<Rol>, IRol
     
     public RolRepository(ApiContext context) : base(context)
     {
-       _context = context;
+        _context = context;
     }
     public override async Task<IEnumerable<Rol>> GetAllAsync()
     {
@@ -41,49 +41,4 @@ public class RolRepository : GenericRepo<Rol>, IRol
 
         return (totalRegistros, registros);
     }
-/*
-    public async Task<object> Consulta2B()
-    {
-        
-        var Movimiento = await (
-            from d in _context.DetalleMovimientos
-            join m in _context.MovimientoMedicamentos on d.IdMovimientoMedicamentoFk equals m.Id
-            
-            select new{
-                idMovimientoMedicamento = m.Id,
-                TipoMovimiento = m.TipoMovimiento.Descripcion,
-                total = d.Precio * d.Cantidad,
-            }).Distinct()
-            .ToListAsync();
-
-        return Movimiento;
-    }
-    public virtual async Task<(int totalRegistros,object registros)> Consulta2B(int pageIndez, int pageSize, string search)
-    {
-        var query = 
-            (
-            from d in _context.DetalleMovimientos
-            join m in _context.MovimientoMedicamentos on d.IdMovimientoMedicamentoFk equals m.Id
-            
-            select new{
-                idMovimientoMedicamento = m.Id,
-                TipoMovimiento = m.TipoMovimiento.Descripcion,
-                total = d.Precio * d.Cantidad,
-            }).Distinct();
-        
-       if(!string.IsNullOrEmpty(search))
-        {
-            query = query.Where(p => p.idMovimientoMedicamento.ToString().ToLower().Contains(search));
-        }
-
-        query = query.OrderBy(p => p.idMovimientoMedicamento);
-        var totalRegistros = await query.CountAsync();
-        var registros = await query 
-            .Skip((pageIndez - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync();
-
-        return (totalRegistros, registros);
-    } 
- */
 }
