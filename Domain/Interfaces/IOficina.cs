87517d52bc9d90b380e02780fd582d482b5f5e85
DevJLeon <celisjoshleon@gmail.com>
2023-11-22 15:20:08 -0500

    minor fix

--- Domain/Interfaces/IOficina.cs
+++ Domain/Interfaces/IOficina.cs
@@ -4,5 +4,5 @@ namespace Domain.Interfaces;
 public interface IOficina : IGenericRepo<Oficina>
 {
     Task<IEnumerable<object>> Consulta26();
-    Task<(int totalRegistros, object registros)> Consulta26(int pageIndez, int pageSize, string search)
+    Task<(int totalRegistros, object registros)> Consulta26(int pageIndez, int pageSize, string search);
 }
